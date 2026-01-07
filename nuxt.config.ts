1f1434a60283b8dad8578c85c7cd00e23a1b267e
Christiaan Hendriksen <christiaan.hendriksen@gmail.com>
2026-01-07 10:35:05 +0000

    Deploy as static site on Vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
   ],
+  nitro: {
+    preset: 'vercel_static',
+  },
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
 })
